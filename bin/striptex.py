9599801428d4a6ec0343f269c2f19c73990aec7e
Shawn Meier <shawn.meier@colorado.edu>
2022-04-15 14:30:26 -0600

    remove begin and end environments

--- bin/striptex.py
+++ bin/striptex.py
@@ -1,6 +1,7 @@
 #!/Users/shawnmeier/.pyenv/versions/3.6.5/bin/python3.6
 ##Script for removing latex commands
 # activate with \c in vim visual mode (see vimrc in vim directory)
+from pylatexenc.latexwalker import LatexWalker
 import sys
 import os
 import re
@@ -15,12 +16,16 @@ def removeCmdLeaveContents(cmdname, v):
 	return re.sub(r'\\%s{([^}]*)}' % cmdname, r' \1 ',v)
 
 def replaceCmdIgnoreContents(cmdname,replacement,v):
-	return re.sub(r'\\%s' % cmdname, r' replacement ',v)
+	return re.sub(r'\\%s' % cmdname, r' %s ' % replacement,v)
 # replace ref with contents
 output = re.sub(r'\\ref{([^}:]*):([^}:]*)}',r'\1 \2', output)
 output = re.sub(r'\\JEDI{[^}]*}',r'',output)
+output = re.sub(r'\\begin{[^}]*}',r'',output)
+output = re.sub(r'\\end{[^}]*}',r'',output)
 output = removeCmdLeaveContents('emph',output)
 output = replaceCmdIgnoreContents('wfhistjudge', 'math h',output)
 output = replaceCmdIgnoreContents('citet','', output)
 output = re.sub(r'\$','',output)
+output = re.sub(r'\%','',output)
+output = removeCmdLeaveContents('codej',output)
 os.system("say \"%s\"" % output)
