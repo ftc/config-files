321730ee7f993d6bc217d21f66447f2a038ed187
Shawn Meier <shawn.meier@colorado.edu>
2022-04-21 13:11:25 -0600

    script to find what is using cpu while away

---
+++ bin/sleepProf.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+while true;
+do
+	date '+%s' > $1 #log time to file
+	ps aux | sort -nrk 3,3 | head -n 5 > $1 #log top 5 cpu using programs to file
+	sleep 900 #sleep 15 minutes
+done
+
