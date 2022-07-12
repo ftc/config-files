eeabde6120dbbbafabd91aeb4d3a746cda183f7e
Shawn Meier <shawn.meier@colorado.edu>
2022-07-12 14:03:07 -0600

    strip comments, factor out methods and lists for easier modification, add cmds

--- bin/striptex.py
+++ bin/striptex.py
@@ -1,7 +1,7 @@
 #!/Users/shawnmeier/.pyenv/versions/3.6.5/bin/python3.6
 ##Script for removing latex commands
 # activate with \c in vim visual mode (see vimrc in vim directory)
-from pylatexenc.latexwalker import LatexWalker
+#from pylatexenc.latexwalker import LatexWalker
 import sys
 import os
 import re
@@ -12,23 +12,31 @@ else:
     output = sys.stdin.read()
 #print(output)
 
+lines = output.split('\n')
+olines = []
+for line in lines:
+    olines.append(line.split('%')[0])
+
+output='\n'.join(olines)
+
 def removeCmdLeaveContents(cmdname, v):
 	return re.sub(r'\\%s{([^}]*)}' % cmdname, r' \1 ',v)
 
 def replaceCmdIgnoreContents(cmdname,replacement,v):
-	return re.sub(r'\\%s' % cmdname, r' %s ' % replacement,v)
+	return re.sub(r'\\%s{[^}]*}' % cmdname, r' %s ' % replacement,v)
 # replace ref with contents
-output = re.sub(r'\\ref{([^}:]*):([^}:]*)}',r'\1 \2', output)
-output = re.sub(r'\\JEDI{[^}]*}',r'',output)
-output = re.sub(r'\\begin{[^}]*}',r'',output)
-output = re.sub(r'\\end{[^}]*}',r'',output)
-output = re.sub(r'\\citet{[^}]*}',r'',output)
-output = re.sub(r'\\cite{[^}]*}',r'',output)
-output = removeCmdLeaveContents('emph',output)
-output = replaceCmdIgnoreContents('wfhistjudge', 'math h',output)
-#output = replaceCmdIgnoreContents('citet','', output)
-#output = replaceCmdIgnoreContents('cite','', output)
+
+for refcmd in ['ref','autoref','figref','thmref']:
+    # remove colon in references
+    output = re.sub(r'\\%s{([^}:]*):([^}:]*)}' % refcmd,r'\1 \2', output)
+    output = re.sub(r'\\%s{([^}:]*)}' % refcmd,r'\1', output)
+
+for ignoreContentCmd in ['JEDI','begin','end','citet','cite', 'wfhistjudge']:
+    output = replaceCmdIgnoreContents(ignoreContentCmd,'',output)
+
+for keepContentCmd in ['emph', 'codej']:
+    output = removeCmdLeaveContents(keepContentCmd, output)
+
 output = re.sub(r'\$','',output)
 output = re.sub(r'\%','',output)
-output = removeCmdLeaveContents('codej',output)
 os.system("say \"%s\"" % output)
